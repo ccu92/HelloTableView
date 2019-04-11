c7ec6d02532e81a2cafa09c1c18215080c16e859
ccu92 <yenping.tsao@gmail.com>
2019-04-11 14:23:13 +0800

    HelloTableView

--- HelloTableView/ViewController.swift
+++ HelloTableView/ViewController.swift
@@ -7,8 +7,21 @@
 //
 
 import UIKit
+var rolesArray = ["小飛象", "小老鼠", "馬戲團帳篷", "花生", "羽毛"]
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITextViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rolesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cellIdentifier = "Cell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+        cell.textLabel?.text = rolesArray[indexPath.row]
+        cell.imageView?.image = UIImage(named: rolesArray[indexPath.row])
+        return cell
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
